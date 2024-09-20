053a43cab22342142c8ef7aa61ec1bc5a3e36a11
gajimenez7 <gajimenez7@miners.utep.edu>
2024-09-20 13:54:55 -0600

    new shape and font print

--- draw_shapes.c
+++ draw_shapes.c
@@ -23,6 +23,42 @@ void print_triangle(int leftCol, int size)
     for (col = 0; col < minCol; col++) putchar(' ');
     for (       ; col <= maxCol; col++) putchar('*');
     putchar('\n');
-  }
+     }
+}
+
+void print_arrow(int leftCol, int size)
+{
+  int col;
+  for (int row = 0; row <= size; row++)
+    {
+      int minCol, maxCol;
+      minCol = leftCol + size - row;
+      maxCol = leftCol + size + row;
+
+      for (col = 0; col < minCol; col++)
+	{
+	  putchar(' ');
+	}
+      for (       ; col <= maxCol; col++)
+	{
+	  putchar('*');
+	}
+      putchar('\n');
+    }
+
+  leftCol = leftCol + (size / 2);
+  int endCol = leftCol + size;
+  for (int row = 2; row < size; row++)
+    {
+      for (col = 0; col < leftCol; col++)
+	{
+	  putchar(' ');
+	}
+      for (      ; col < endCol; col++)
+	{
+	  putchar('*');
+	}
+      putchar('\n');
+    }
 }
 
